adc55c2f55e1b13afbbd29cc7afaa7ba79a0e4c1
Ronnie Ghose <1313566+RONNCC@users.noreply.github.com>
2025-08-19 01:21:27 -0700

    Add devtools-mode command in package.json

--- start-me.sh
+++ start-me.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Interview mode: fast, clean startup (no DevTools). Use this for interviews/demos.
-# pnpm run intv
+pnpm run intv
 
 # For development (watch mode, auto-installs), run manually:
-pnpm run dev
+# pnpm run dev
